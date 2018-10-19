8fc0070791d54806d64efaca95007c307c968ec4
Juan Tenezaca <tenj34@gmail.com>
2018-10-19 03:27:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,9 @@ class App < Sinatra::Base
   get "/" do
     erb :user_input
   end
+
+  post '/piglatinize' do
+   @phrase = params[:user_phrase]
+   PigLatinizer.new.piglatinize(@phrase)
+ end
 end
